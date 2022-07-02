ac0a0821afe5cdddd08946a07370482cb1afa5b2
tonytw <tonytw9776@gmail.com>
2022-07-02 11:07:59 +0800

    feat : demo test-01

---
+++ ex5-js-mdntutorial/main.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+  const para = document.querySelector("p");
+  para.addEventListener("click", updateName);
+
+  function updateName() {
+    const name = prompt("Enter a new name");
+    para.textContent = `Player 1: ${name}`;
+  }
+};
