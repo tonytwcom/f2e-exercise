ec7cb7fbc06e2b91a0dde05c6c4debd7b55aa9aa
tonytw <tonytw9776@gmail.com>
2022-07-04 14:05:01 +0800

    feat : add change function of the image

--- ex5-js-mdntutorial/test-03/main.js
+++ ex5-js-mdntutorial/test-03/main.js
@@ -1,5 +1,6 @@
-function myfn() {
-  document.getElementById("myimg").src = "./image/img-2.jpg";
+function coverImg() {
+  document.getElementById("myimg").src = "../img/img-2.png";
 }
-
-
+function recoverImg() {
+  document.getElementById("myimg").src = "https://picsum.photos/200/300";
+}
