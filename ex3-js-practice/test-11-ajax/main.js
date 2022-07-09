ab16de66c7337d9ac078caf74f2d9cceed62076f
tonytw <tonytw9776@gmail.com>
2022-07-09 08:51:33 +0800

    feat : add ajax method

--- ex3-js-practice/test-11-ajax/main.js
+++ ex3-js-practice/test-11-ajax/main.js
@@ -1,8 +1,8 @@
 function readfile() {
   const xhttp = new XMLHttpRequest();
   xhttp.onload = function () {
-    document.getElementById("demo").innerHTML = this.responseText;
+    document.getElementById("demo").innerHTML = this.responseText ;
   };
-  xhttp.open("GET", ".txt", true);
+  xhttp.open("GET", "./salary102.json", true);
   xhttp.send();
 }
