04ab5755a789fe87067861fa8fe231f9a72b5e6e
tonytw <tonytw9776@gmail.com>
2022-07-19 09:41:21 +0800

    chore : change folder

---
+++ ex5-amos-tutorial/test-11-jquery/main.js
@@ -0,0 +1,11 @@
+// $(function () {
+//     $('').text('Hello World');
+// });
+
+// let x = Boolean("111");
+var rs = 100;
+
+// console.log(1 < 2 && 8 < 7);
+console.log('ABC' < 'aBC');
+// console.log('x is '+x);
+// console.log(typeof x);
